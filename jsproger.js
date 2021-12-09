1454bf97c0484650689da8050849270cb9e288e2
DmitriyManzharov <95544212+DmitriyManzharov@users.noreply.github.com>
2021-12-09 16:14:08 +0300

    Add files via upload

---
+++ jsproger.js
@@ -0,0 +1,6 @@
+"use strict"
+const circle = document.querySelector(".circle");
+const square = document.querySelector(".square");
+square.addEventListener("click", function(){square.style.background="blue"});
+circle.addEventListener("click", function(){circle.style.background="yellow"});
+
